b2ad91cc8771983f906173b9ca24df230af8d7fd
Rob Lord <robertlord@outlook.com>
2026-01-31 12:35:51 -0800

    lab completed

--- server/app.py
+++ server/app.py
@@ -17,5 +17,47 @@ class ClearSession(Resource):
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 
+user_schema = UserSchema()
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'])
+        user.password_hash = data['password']
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return user_schema.dump(user), 201
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user_schema.dump(user), 200
+        return {}, 204
+
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data['username']).first()
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return user_schema.dump(user), 200
+        return {'error': 'Invalid credentials'}, 401
+
+api.add_resource(Login, '/login', endpoint='login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
